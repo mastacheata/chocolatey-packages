f4feaba17f25eabe2b45c3525f80ceece5eeb640
Benedikt Bauer <benedikt@xenzilla.de>
2016-05-18 18:10:21 +0200

    Add generated install/uninstall ps1 files

---
+++ phpstorm/tools/chocolateyInstall.ps1
@@ -0,0 +1,6 @@
+Install-ChocolateyPackage `
+  -PackageName 'phpstorm' `
+  -FileType 'EXE' `
+  -Silent '/S' `
+  -Checksum '7217c754a0659ab5079224b20cf1f042' `
+  -Url 'https://download.jetbrains.com/webide/PhpStorm-2016.1.1.exe'
