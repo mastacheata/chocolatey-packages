da3ec778240d3c5dbfe0e6ebc15ccd64a077ab86
Benedikt Bauer <benedikt@xenzilla.de>
2017-03-15 15:51:09 +0100

    Remove generated Installer/Uninstaller Scripts

--- rubymine/tools/chocolateyInstall.ps1
+++
@@ -1,7 +0,0 @@
-Install-ChocolateyPackage `
-  -PackageName 'rubymine' `
-  -FileType 'EXE' `
-  -Silent '/S' `
-  -ChecksumType 'sha256'
-  -Checksum 'd8dd0e3368441d23d882b9d155930a1cd660234f1c1bf7ca11dc7d1c16cbbecd' `
-  -Url ''
--- rubymine/tools/chocolateyUninstall.ps1
+++
@@ -1,9 +0,0 @@
-$tools = Split-Path $MyInvocation.MyCommand.Definition
-
-. $tools\uninstall.ps1
-
-Uninstall-ChocolateyPackage `
-  -PackageName 'rubymine' `
-  -FileType 'EXE' `
-  -Silent '/S' `
-  -File (Get-Uninstaller -Name 'JetBrains RubyMine 2016.1.1')
