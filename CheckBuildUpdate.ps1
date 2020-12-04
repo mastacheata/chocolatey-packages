04a7cec32a6592e2e59036dfcb8ee0a7d507e427
Benedikt Bauer <benedikt@xenzilla.de>
2020-12-04 01:05:46 +0100

    Build EAP packages as well

--- CheckBuildUpdate.ps1
+++ CheckBuildUpdate.ps1
@@ -12,22 +12,40 @@ $directory = Split-Path $MyInvocation.MyCommand.Definition
 . $directory\tools\helper.ps1
 
 # If nuspec exists, grab the old version number
-if (Test-Path "$directory\rubymine.nuspec") {
-    Write-Host "Found cached rubymine.nuspec"
-    $oldVersion = (([xml](Get-Content "$directory\rubymine.nuspec")).package.metadata.version)
+if (Test-Path "$directory\rubymine-eap.nuspec") {
+    Write-Host "Found cached rubymine-eap.nuspec"
+    $oldVersion = (([xml](Get-Content "$directory\rubymine-eap.nuspec")).package.metadata.version)
+    $versions = $oldVersion -Split '-EAP'
+    if ($versions.Length -gt 1) {
+        $oldBuild = $versions[1] -replace('-', '.')
+        $oldEap = $true
+    }
+    else {
+        $oldBuild = $oldVersion
+        $oldEap = $false
+    }
 } else {
-    Write-Host "Couldn't find cached rubymine.nuspec at $directory\rubymine.nuspec"
+    Write-Host "Couldn't find cached rubymine-eap.nuspec at $directory\rubymine-eap.nuspec"
     $oldVersion = "0.0.0"
+    $oldBuild = "0.0.0"
+    $oldEap = $false
 }
 # Get new version number from release API
-$release = (Invoke-RestMethod -Uri 'https://data.services.jetbrains.com/products/releases?code=RM&latest=true&type=release' -UseBasicParsing)
-$newVersion = $release.RM.version
-$majorVersion = $release.RM.majorVersion
+$release = (Invoke-RestMethod -Uri 'https://data.services.jetbrains.com/products/releases?code=RM&latest=true&type=eap' -UseBasicParsing)
+$isEap = $release.RM.type -eq 'eap'
+if ($release.RM.type -eq 'eap') {
+    $newVersion = "$($release.RM.version)-EAP$($release.RM.build -replace('\.', '-'))"
+    $newBuild = $release.RM.build
+}
+else {
+    $newVersion = $release.RM.version
+    $newBuild = $release.RM.version
+}
 
-Write-Host "Version compare: old: $($oldVersion) new: $($newVersion)"
+Write-Host "Version compare: old: $($oldBuild) new: $($newBuild)"
 # Compare versions, only proceed if new version is real smaller than old version
-if (([version]$oldVersion -lt [version]$newVersion) -or $force) {
-    Write-Host "Cached rubymine.nuspec not found or web version differs from cache"
+if (([version]$oldBuild -lt [version]$newBuild) -or $force -or ($oldEap -ne $isEap)) {
+    Write-Host "Cached rubymine-eap.nuspec not found or web version differs from cache"
     # If the version appears new to us, but is already on chocolatey.org, ignore it
     try {
         Write-Host "Check if Version is already released on chocolatey.org"
@@ -59,8 +77,7 @@ else {
 # If not fallback to a documentation page that contains a list of all release note pages.
 try {
     Write-Host "Test version specific releaseNotes url"
-    $release_url = "https://confluence.jetbrains.com/display/RUBYDEV/RubyMine+$($majorVersion)"
-    Write-Host $release_url
+    $release_url = $release.RM.notesLink
     $response = Invoke-WebRequest -Uri $release_url -method head
 } catch {
     Write-Host ">>`tERROR:`t`tVersion specific releaseNotes url test failed" -foreground "red"
@@ -83,8 +100,9 @@ Write-Host "Update nuspec"
 [xml]$nuspec_template = (Get-Content .\template.nuspec)
 $nuspec_template.package.metadata.version = $newVersion
 $nuspec_template.package.metadata.releaseNotes = $release_url
-if (Test-Path "$directory\rubymine.nuspec") { Remove-Item "$directory\rubymine.nuspec" }
-$nuspec_template.save("$directory\rubymine.nuspec")
+$nuspec_template.package.metadata.description = $nuspec_template.package.metadata.description -replace ('{{release}}', $release.RM.whatsnew)
+if (Test-Path "$directory\rubymine-eap.nuspec") { Remove-Item "$directory\rubymine-eap.nuspec" }
+$nuspec_template.save("$directory\rubymine-eap.nuspec")
 
 Write-Host "Update Installer Powershell script with new URL and checksum"
 if (Test-Path "$directory\tools\chocolateyInstall.ps1") { Remove-Item "$directory\tools\chocolateyInstall.ps1" }
@@ -94,6 +112,6 @@ if (Test-Path "$directory\tools\chocolateyUninstall.ps1") { Remove-Item "$direct
 (Get-Content "$directory\tools\chocolateyUninstall_template.ps1") -replace('{{version}}', $newVersion) | Set-Content "$directory\tools\chocolateyUninstall.ps1"
 
 # Pack Nupkg file
-choco pack "$directory\rubymine.nuspec"
+choco pack "$directory\rubymine-eap.nuspec"
 
 $env:RUBYMINE_VERSION=$newVersion
